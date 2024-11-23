e36e97ffc3715fd6eb4676067a3220d7e35d18cb
Felipe <felipeclave@gmail.com>
2024-11-23 10:59:37 -0300

    novos componentes e importação de fontes para os estilos globais

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import ItemNavegacao from "../ItemNavegacao"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,9 +13,41 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href=""> Início</a>
-                    </li>
+                    <ItemNavegacao
+                        iconeAtivo='/icones/home-ativo.png'
+                        iconeInativo='/icones/home-inativo.png'
+                        ativo={true}
+                    >
+                        Inicio
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo='/icones/mais-vistas-ativo.png'
+                        iconeInativo='/icones/mais-vistas-inativo.png'
+                        ativo={false}
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo='/icones/mais-curtidas-ativo.png'
+                        iconeInativo='/icones/mais-curtidas-inativo.png'
+                        ativo={false}
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo='/icones/novas-ativo.png'
+                        iconeInativo='/icones/novas-inativo.png'
+                        ativo={false}
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo='/icones/surpreenda-me-ativo.png'
+                        iconeInativo='/icones/surpreenda-me-inativo.png'
+                        ativo={false}
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
